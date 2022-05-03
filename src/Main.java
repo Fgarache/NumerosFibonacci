b527c5db11a022ced7ed6a7758f0e57c82430744
Francisco Garache <franciscogarach3@gmail.com>
2022-05-03 10:31:50 -0600

    1

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        for (int i = 1; i < 5; i++) {
+            System.out.println(""+main.num(i));
+        }
+        System.out.printf("recursividad"+ main.fibonacci(2));
+
+    }
+    public int num (int num1) {
+        if (num1 == 1 || num1==2) {
+            return 1;
+        }else {
+            return num(num1-1)+num(num1-2);
+        }
+    }
+
+
+
+    int fibonacci(int n)
+    {
+        if (n>1){
+            return fibonacci(n-1) + fibonacci(n-2);  //función recursiva
+        }
+        else if (n==1) {  // caso base
+            return 1;
+        }
+        else if (n==0){  // caso base
+            return 0;
+        }
+        else{ //error
+            System.out.println("Debes ingresar un tamaño mayor o igual a 1");
+            return -1;
+        }
+    }
+
+}
+
+
+
